c279b2d5172614728efd07c844c04fdb03b244e9
DocLock17 <michael.c.locker@gmail.com>
2022-09-07 20:55:08 -0700

    relaxed wrist

--- PCA9685_ArmBot/servoTest2.py
+++ PCA9685_ArmBot/servoTest2.py
@@ -298,17 +298,21 @@ def test3():
 def base_posture(hold_time=2):
     """Declare stiff arm"""
     # Base Shoulder (lower numbers extend arm away from bot)
-    set_servo_angle(5, 140)
+    set_servo_angle(1)
     time.sleep(0.1)
-
-
-    # Lower Elbow (lower numbers lifts up) DIstal
-    set_servo_angle(1,180)
+    
+    set_servo_angle(5, 140)
     time.sleep(0.1)
 
     # Upper Elbow (Lower numbers lowers arm or Contracts) Medial
     set_servo_angle(8, 25)
     time.sleep(0.1)
+
+    # Lower Elbow (lower numbers lifts up) DIstal
+    set_servo_angle(1,180)
+    time.sleep(0.1)
+
+
 
     # Base Rotation (Lower numbers move to Bots right)
     set_servo_angle(10, 110)
