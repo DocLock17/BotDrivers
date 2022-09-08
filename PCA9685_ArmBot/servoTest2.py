2c7b74145b1f7eb80a5e37cd9ce7b579e86ff4fc
DocLock17 <michael.c.locker@gmail.com>
2022-09-07 20:53:02 -0700

    adjustments1

--- PCA9685_ArmBot/servoTest2.py
+++ PCA9685_ArmBot/servoTest2.py
@@ -301,12 +301,13 @@ def base_posture(hold_time=2):
     set_servo_angle(5, 140)
     time.sleep(0.1)
 
-    # Upper Elbow (Lower numbers lowers arm or Contracts) Medial
-    set_servo_angle(8, 25)
-    time.sleep(0.1)
 
     # Lower Elbow (lower numbers lifts up) DIstal
     set_servo_angle(1,180)
+    time.sleep(0.1)
+
+    # Upper Elbow (Lower numbers lowers arm or Contracts) Medial
+    set_servo_angle(8, 25)
     time.sleep(0.1)
 
     # Base Rotation (Lower numbers move to Bots right)
