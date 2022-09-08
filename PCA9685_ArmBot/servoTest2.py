130d1b4e14238bd7ae38f7b587065aaef4061cfa
DocLock17 <michael.c.locker@gmail.com>
2022-09-07 20:31:46 -0700

    adjustments 6

--- PCA9685_ArmBot/servoTest2.py
+++ PCA9685_ArmBot/servoTest2.py
@@ -104,9 +104,9 @@ def main():
     move_to_angle(5, 35, 20)
     sleep(.5)
 
-    move_to_angle(8, 125, 145)
+    move_to_angle(8, 125, 140)
     sleep(.5)
-    move_to_angle(5, 30, 5)
+    move_to_angle(5, 30, 3)
     sleep(.5)
 
     sleep(10)
